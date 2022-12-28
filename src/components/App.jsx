15e2fe40ac69078157e3479c5fb19ee98cc62d13
ChavezDev <chavezdev.41th@gmail.com>
2022-12-27 20:15:52 -0500

    Iniciamos el manejo de variables de entorno

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,10 @@
+import { API_URL } from "../constants/dev"
 
 function App() {
 
   return (
     <div>
-      <h1>FUNCIONA vamos bien</h1>
+      <h1>FUNCIONA vamos bien esta sera nuesta url: {API_URL}</h1>
     </div>
   )
 }
